379bca7e5f9776d1a78c77d1f660dbbb2049e7e2
fish-chuan <newdavidlin@gmail.com>
2020-01-17 12:13:29 +0800

    Add H ome page css

--- LentSystem/urls.py
+++ LentSystem/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from lent.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('lent.urls'))
+    path('', index)
 ]
